6d3e3b115e703f58a8bc0cb53065a904b4354bfd
Mylene Leroy <myleneleroy@macbook-air.home>
2024-09-16 05:03:21 -0700

    feat: update card

--- components/KanbanBoard/components/Task.tsx
+++ components/KanbanBoard/components/Task.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
-import styles from './Task.module.css';
 
 type TaskProps = {
     task: {
         id: string;
         title: string;
         description: string | null;
-        status: 'TODO' | 'IN_PROGRESS' | 'DONE';
         updatedAt: string;
     };
     index: number;
@@ -18,18 +16,19 @@ const Task: React.FC<TaskProps> = ({ task, index }) => {
         <Draggable draggableId={task.id} index={index}>
             {(provided) => (
                 <div
+                    className='mb-2'
                     ref={provided.innerRef}
                     {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                >
-                    <div className={`${styles['task-card']} ${styles['task-status']} ${styles[task.status.toLowerCase()]}`}>
-                        <div className={styles['task-content']}>
-                            <h3 className={styles['task-title']}>{task.title}</h3>
-                            <p className={styles['task-description']}>{task.description || 'No description available'}</p>
-                            <div className={styles['task-meta']}>
-                                <p><strong>Updated At:</strong> {new Date(task.updatedAt).toLocaleDateString()}</p>
-                            </div>
+                    {...provided.dragHandleProps}>
+                    <div
+                        className="block max-w-[18rem] rounded-lg border bg-white text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+                        <div className="border-b-2 border-neutral-100 px-6 py-3 dark:border-white/10">{task.title}</div>
+                        <div className="p-6">
+                            <p className="text-base text-success-600">
+                                {task.description || 'No description available'}
+                            </p>
                         </div>
+                        <div className="border-t-2 border-success-600 px-6 py-3 text-xs text-slate-500">{new Date(task.updatedAt).toLocaleDateString()}</div>
                     </div>
                 </div>
             )}
